babd8e73637919f2e16e1a8cf135859d30f38e5d
Teo Maragakis <teomaragakis@gmail.com>
2015-07-03 13:00:01 +0200

    Improved comment form
    Added bootstrap classes to comment form. Needs to be organized into columns.

--- comments.php
+++ comments.php
@@ -50,8 +50,34 @@ if ( post_password_required() ) {
 		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentyfifteen' ); ?></p>
-	<?php endif; ?>
+	<?php endif;
 
-	<?php comment_form(); ?>
+	$comment_args = array(
+  	'class_submit' => 'btn btn-default',
+  	'comment_field' =>  '<div class="form-group comment-form-comment"><label for="comment" class="control-label">' . _x( 'Comment', 'noun' ) .
+    '</label><textarea id="comment" class="form-control" name="comment" rows="3" aria-required="true">' .
+    '</textarea></div>',
+    'fields' => array(
+
+  'author' =>
+    '<div class="form-group comment-form-author"><label class="control-label" for="author">' . __( 'Name', 'shoelace' ) . '</label> ' .
+    ( $req ? '<span class="required">*</span>' : '' ) .
+    '<input id="author" class="form-control" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
+    '"' . $aria_req . ' /></div>',
+
+  'email' =>
+    '<div class="form-group comment-form-email"><label class="control-label" for="email">' . __( 'Email', 'shoelace' ) . '</label> ' .
+    ( $req ? '<span class="required">*</span>' : '' ) .
+    '<input id="email" class="form-control" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
+    '"' . $aria_req . ' /></div>',
+
+  'url' =>
+    '<div class="form-group comment-form-url"><label class="control-label" for="url">' . __( 'Website', 'shoelace' ) . '</label>' .
+    '<input id="url" class="form-control" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
+    '" /></div>',
+),
+	);
+
+	comment_form($comment_args); ?>
 
 </div><!-- .comments-area -->
