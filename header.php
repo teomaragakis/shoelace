11318f0c38e761c5631008ebde944208ad0aa4f5
Teo Maragakis <teomaragakis@gmail.com>
2015-07-03 14:03:28 +0200

    Favicon!
    Used a more acceptable default favicon in png format.

--- header.php
+++ header.php
@@ -22,7 +22,7 @@
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<link href="<?php echo get_template_directory_uri(); ?>/favicon.ico" rel="shortcut icon" />
+	<link href="<?php echo get_template_directory_uri(); ?>/favicon.png" rel="shortcut icon" />
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js"></script>
 	<![endif]-->
